b0b95bd811b1e8406e953ac37b0960c6c0a0f6f8
Engkos97 <110719940+engkoskostaman97@users.noreply.github.com>
2024-10-30 14:21:59 +0700

    Update api.jsx

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export const API = axios.create(
+  // {
+  //   baseURL: "https://virtuous-energy-production.up.railway.app/api/v1",
+  // }
   {
-    baseURL: "https://virtuous-energy-production.up.railway.app/api/v1",
+    baseURL: "http://localhost:8080/api/v1",
   }
   //   {
   //   baseURL: process.env.REACT_APP_BASEURL,
